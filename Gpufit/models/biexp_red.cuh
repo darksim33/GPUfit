5d4915f0461543c21a58868a5b468bc232b33c91
darksim33 <thomas.thiel92@gmail.com>
2025-11-04 15:07:47 +0100

    fead: fix reduced model to match triexp with last component beeing reduced

--- Gpufit/models/biexp_red.cuh
+++ Gpufit/models/biexp_red.cuh
@@ -7,16 +7,11 @@
 * This function calculates the values of reduced biexponential functions
 * and their partial derivatives with respect to the model parameters.
 *
-* The reduced biexponential function is: S/S0 = (1-a)*exp(-b*x) + a*exp(-c*x)
-* The derivatives are:
-* dy/da = exp(-c*x) - exp(-b*x)
-* dy/db = (a-1)*x*exp(-b*x)
-* dy/dc = -a*x*exp(-c*x)
 * The reduced biexponential function is: S/S0 = a*exp(-b*x) + (1-a)*exp(-c*x)
 * The derivatives are:
-* dy/da = exp(-c*x) - exp(-b*x)
-* dy/db = a*(-x)*exp(-b*x)
-* dy/dc = (1-a)*(-x)*exp(-c*x)
+* dy/da = (exp(-b*x) - exp(-c*x))
+* dy/db = -a*x*exp(-b*x)
+* dy/dc = (a-1)*x*exp(-c*x)
 *
 * This function makes use of the user information data to pass in the
 * independent variables (X values) corresponding to the data.  The X values
@@ -117,12 +112,12 @@ __device__ void calculate_biexp_red(
     /* value
     S/S0 = a*exp(-b*x) + (1-a)*exp(-c*x)
     p[0]: a (f2)   p[1]: b  (D1)   p[2]: c   (D2)     */
-    value[point_index] = (1.0 - p[0]) * exp(-p[1]*x) + p[0]*exp(-p[2]*x);
+    value[point_index] =  p[0] * exp(-p[1]*x) + (1.0 - p[0]) *exp(-p[2]*x);
 
     /* derivatives
-    dy/da = exp(-c*x) - exp(-b*x)
-    dy/db = a*(-x)*exp(-b*x)
-    dy/dc = (1-a)*(-x)*exp(-c*x)
+    dy/da = (exp(-b*x) - exp(-c*x))
+    dy/db = -a*x*exp(-b*x)
+    dy/dc = (a-1)*x*exp(-c*x)
     p[0]: a (f2)   p[1]: b  (D1)   p[2]: c   (D2)     */
 
     REAL* current_derivatives = derivative + point_index;
